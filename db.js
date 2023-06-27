279b75048de7bb91639d0d29ff4bd0b1d91da65f
Dhairya5122 <shahdhairya101@gmail.com>
2023-06-28 01:28:55 +0530

    dhiau

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 mongoose
-  .connect("mongodb://0.0.0.0:27017/jsfashion")
+  .connect("mongodb://127.0.0.1:27017/jsfashion")
   .then(() => {
     console.log("Connected to database");
   })
